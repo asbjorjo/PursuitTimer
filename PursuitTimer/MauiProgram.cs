d469a54984ac451280847d744e091896b1f6b2bf
Asbjørn L. Johansen <asbjorjo@gmail.com>
2022-06-27 11:16:41 +0200

    Register Roboto mono with application.

--- PursuitTimer/MauiProgram.cs
+++ PursuitTimer/MauiProgram.cs
@@ -13,6 +13,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("RobotoMono-VariableFont_wght.ttf", "RobotoMono");
 			});
 
 		builder.Services.AddSingleton<MainPage>();
