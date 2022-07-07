e0f9d205cf508de210b3822307b451b2f52492bd
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-07-07 22:13:26 +0200

    Introduce model class for split times

---
+++ PursuitTimer.Shared/Model/SplitTime.cs
@@ -0,0 +1,18 @@
+﻿namespace PursuitTimer.Shared.Model
+{
+    public class SplitTime
+    {
+        public DateTime Time { get; }
+        public TimeSpan Split { get; }
+        public TimeSpan DeltaPrevious { get; }
+        public TimeSpan DeltaTarget { get; }
+
+        public SplitTime(DateTime time, TimeSpan split, TimeSpan deltaPrevious, TimeSpan deltaTarget = default)
+        {
+            Time = time;
+            Split = split;
+            DeltaPrevious = deltaPrevious;
+            DeltaTarget = deltaTarget;
+        }
+    }
+}
