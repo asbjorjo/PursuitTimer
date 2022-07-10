4fe64c79f9ca73553d9f25f42b8f901580271855
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-07-10 14:21:23 +0200

    Introduce view model for summary.

---
+++ PursuitTimer.Shared/Model/SummaryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using PursuitTimer.Shared.Extensions;
+
+namespace PursuitTimer.Shared.Model
+{
+    public class SummaryViewModel
+    {
+        public SummaryViewModel()
+        {
+            SplitTimes = new();
+        }
+
+        public SummaryViewModel(List<SplitTime> splitTimes)
+        {
+            SplitTimes = splitTimes;
+        }
+
+        public List<SplitTime> SplitTimes
+        {
+            get;
+        }
+
+        public TimeSpan SumTimes
+        {
+            get => SplitTimes.Select(x => x.Split).Sum();
+        }
+    }
+}
