68dbef97735ac114e665f4d9debe39ad57c7f6a1
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-07-10 21:51:05 +0200

    Note start time and use for first split.

--- PursuitTimer.Shared/Services/TimerService.cs
+++ PursuitTimer.Shared/Services/TimerService.cs
@@ -5,7 +5,7 @@ namespace PursuitTimer.Shared.Services
     public class TimerService
     {
         private bool running = false;
-        private List<DateTime> times = new();
+        private DateTime StartTime;
         private List<SplitTime> splits = new();
 
         public List<SplitTime> Splits
@@ -16,6 +16,7 @@ namespace PursuitTimer.Shared.Services
         public void Start()
         {
             Reset();
+            StartTime = DateTime.UtcNow;
             running = true;
         }
 
@@ -24,45 +25,22 @@ namespace PursuitTimer.Shared.Services
             running = false;
         }
 
-        public long MarkTime()
+        public void MarkTime()
         {
             if (!running)
             {
                 Start();
-            }
+            } else {
+                var time = DateTime.UtcNow;
+                var split = Splits.Count > 0 ? time - splits.Last().Time : time - StartTime;
 
-            var time = DateTime.UtcNow;
-
-            if (times.Count > 0)
-            {
-                var split = time - times.Last();
-                
                 splits.Add(new SplitTime(time, split, splits.Count > 0 ? split - splits.Last().Split : default));
             }
-            
-            times.Add(time);
-
-            int n = times.Count;
-
-            return n > 1 ? ((long)(times[n - 1] - times[n - 2]).TotalMilliseconds) : 0;
-        }
-
-        public List<TimeSpan> GetTimes()
-        {
-            List<TimeSpan> splittimes = new List<TimeSpan>();
-
-            for (int i = 1; i < times.Count; i++)
-            {
-                splittimes.Add(times[i] - times[i - 1]);
-            }
-
-            return splittimes;
         }
 
         public void Reset()
         {
             Stop();
-            times = new();
             splits = new();
         }
 
