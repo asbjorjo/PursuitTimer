fbdd396dfd6522f51222ac9a8092429bd6c7db7e
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-07-24 12:11:58 +0200

    Use data template selector to formart splits in summary

---
+++ PursuitTimer/Templates/SplitSummaryDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using PursuitTimer.Model;
+
+namespace PursuitTimer.Templates;
+
+public class SplitSummaryDataTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate PositiveSplit { get; set; }
+    public DataTemplate NegativeSplit { get; set; }
+    public DataTemplate NeutralSplit { get; set; }
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        SplitTime splitTime = (SplitTime)item;
+
+        if  (splitTime.DeltaPrevious > TimeSpan.Zero)
+        {
+            return PositiveSplit;
+        } 
+        else if (splitTime.DeltaPrevious < TimeSpan.Zero)
+        {
+            return NegativeSplit;
+        }
+        else
+        {
+            return NeutralSplit;
+        }
+    }
+}
