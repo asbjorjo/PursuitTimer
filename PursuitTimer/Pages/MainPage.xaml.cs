e6a57aa768630994438c80f8eb0b156bc60cbf6a
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-10-06 21:44:06 +0200

    Add navigation to SummaryPage from main menu

--- PursuitTimer/Pages/MainPage.xaml.cs
+++ PursuitTimer/Pages/MainPage.xaml.cs
@@ -1,15 +1,49 @@
-﻿namespace PursuitTimer.Pages;
+﻿using PursuitTimer.Services;
+using PursuitTimer.ViewModels;
+
+namespace PursuitTimer.Pages;
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-        InitializeComponent();
+    public static readonly BindableProperty ShowSummaryProperty =
+            BindableProperty.Create("ShowSummary", typeof(bool), typeof(MainPage), false);
+    public bool ShowSummary
+    {
+        get => (bool)GetValue(ShowSummaryProperty);
+        set => SetValue(ShowSummaryProperty, value);
     }
 
+    private readonly TimerService _timerService;
+
+    public MainPage(TimerService timerService)
+	{
+        InitializeComponent();
+
+        _timerService = timerService;
+        BindingContext = this;
+    }
+
+    protected override void OnNavigatedTo(NavigatedToEventArgs args)
+    {
+        base.OnNavigatedTo(args);
+
+        ShowSummary = _timerService.TimingSession.SplitTimes.Count > 0;
+    }
     private async void OnSetupClickedAsync(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//TimerSetupPage");
+    }
+
+    private async void OnSumaryClickedAsync(object sender, EventArgs e)
+    {
+        SummaryViewModel summaryView = new(_timerService.TimingSession);
+
+        var navigationParameters = new Dictionary<string, object>
+        {
+            {"SummaryView", summaryView}
+        };
+
+        await Shell.Current.GoToAsync("//SummaryPage", navigationParameters);
     }
 
     private async void OnStartClickedAsync(object sender, EventArgs e)
@@ -17,4 +51,3 @@ public partial class MainPage : ContentPage
         await Shell.Current.GoToAsync("//TimerPage");
     }
 }
-
