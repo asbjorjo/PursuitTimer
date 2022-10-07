e14b1ec6d22eea16158b919fe5803e8d59cf2ca6
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-10-07 19:42:43 +0200

    Add restart button to summary

--- PursuitTimer/Pages/SummaryPage.xaml.cs
+++ PursuitTimer/Pages/SummaryPage.xaml.cs
@@ -1,3 +1,4 @@
+using PursuitTimer.Services;
 using PursuitTimer.ViewModels;
 
 namespace PursuitTimer.Pages;
@@ -5,6 +6,7 @@ namespace PursuitTimer.Pages;
 [QueryProperty(nameof(SummaryView), "SummaryView")]
 public partial class SummaryPage : ContentPage
 {
+    private TimerService _timerService;
     public static readonly BindableProperty SummaryViewProperty =
         BindableProperty.Create("SummaryView", typeof(SummaryViewModel), typeof(SummaryPage), new SummaryViewModel());
 
@@ -17,19 +19,29 @@ public partial class SummaryPage : ContentPage
         }
     }
 
-    public SummaryPage()
+    public SummaryPage(TimerService timerService)
 	{
+        _timerService = timerService;
         InitializeComponent();
         BindingContext = this;
     }
 
-    private async void OnStartClickedAsync(object sender, EventArgs e)
+    private async void OnResumeClickedAsync(object sender, EventArgs e)
     {
+        await Shell.Current.GoToAsync("//TimerPage");
+    }
+
+    private async void OnRestartClickedAsync(object sender, EventArgs e)
+    {
+        _timerService.Reset();
+
         await Shell.Current.GoToAsync("//TimerPage");
     }
 
     private async void OnMainClickedAsync(object sender, EventArgs e)
     {
+        _timerService.Stop();
+
         await Shell.Current.GoToAsync("//MainPage");
     }
 }
