0aa178a3a99e79fe21842f539335ebeec186f8c7
Asbjoern L. Johansen <asbjorjo@gmail.com>
2022-10-10 20:13:49 +0200

    Removing unused property

--- PursuitTimer/Platforms/Android/MainActivity.cs
+++ PursuitTimer/Platforms/Android/MainActivity.cs
@@ -7,4 +7,11 @@ namespace PursuitTimer;
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Platform.Init(this, savedInstanceState);
+
+        this.Window.AddFlags(Android.Views.WindowManagerFlags.Fullscreen);
+    }
 }
