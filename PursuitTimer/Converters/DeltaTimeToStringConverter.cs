c1bf39a327f4536d2c6c39ab9a405a62120f9f07
Asbjoern L. Johansen <asbjorjo@gmail.com>
2024-06-29 11:07:02 +0200

    Add converter for target times

---
+++ PursuitTimer/Converters/DeltaTimeToStringConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace PursuitTimer.Converters;
+
+public class DeltaTimeToStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return ((double)value).ToString(parameter as string);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var toleranceString = value as string;
+        var tolerance = double.Parse(toleranceString.Replace(',', '.'), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+        return tolerance;
+    }
+}
