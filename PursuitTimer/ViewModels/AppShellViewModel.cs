da01b6bfc7d18811819c182fb023790f30cb6ce8
Asbjoern L. Johansen <asbjorjo@gmail.com>
2024-07-17 20:02:37 +0200

    Add ViewModel for AppShell

---
+++ PursuitTimer/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PursuitTimer.ViewModels
+{
+    public partial class AppShellViewModel
+    {
+        public TimerViewModel TimerView { get; }
+
+        public AppShellViewModel(TimerViewModel timerView)
+        {
+            TimerView = timerView;
+        }
+    }
+}
