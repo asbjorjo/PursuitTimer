de6e19c6abba1a5e47bb00ce5f0b533aa8839502
Asbjoern L. Johansen <asbjorjo@gmail.com>
2025-01-01 17:37:53 +0100

    Add popup for changes

---
+++ PursuitTimer/Pages/Popups/ChangesPopup.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace PursuitTimer.Pages.Popups;
+
+public partial class ChangesPopup : Popup
+{
+	public ChangesPopup()
+	{
+		InitializeComponent();
+	}
+}
