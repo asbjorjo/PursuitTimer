f24031945ccd8d954ac58f5b5a05c1db7d7f8133
Asbjørn L. Johansen <asbjorjo@gmail.com>
2025-01-01 20:35:12 +0100

    Do not set preferences as static member

--- PursuitTimer/Helpers/TimingTargetHelper.cs
+++ PursuitTimer/Helpers/TimingTargetHelper.cs
@@ -2,7 +2,6 @@
 
 public static class TimingTargetHelper
 {
-    private static readonly IPreferences _preferences = Preferences.Default;
     private const string SharedName = "me.veloti.pursuittimer";
 
     private const string KeyTarget = "TimingTarget";
@@ -11,6 +10,7 @@ public static class TimingTargetHelper
 
     public static TimingTarget GetTimingTarget()
     {
+        IPreferences _preferences = Preferences.Default;
         double target = _preferences.Get<double>(KeyTarget, default, SharedName);
         double over = _preferences.Get<double>(KeyToleranceOver, default, SharedName);
         double under = _preferences.Get<double>(KeyToleranceUnder, default, SharedName);
@@ -25,6 +25,7 @@ public static class TimingTargetHelper
 
     public static void SaveTimingTarget(TimingTarget value)
     {
+        IPreferences _preferences = Preferences.Default;
         _preferences.Set(KeyTarget, value.Target.TotalSeconds, SharedName);
         _preferences.Set(KeyToleranceOver, value.ToleranceOver.TotalSeconds, SharedName);
         _preferences.Set(KeyToleranceUnder, value.ToleranceUnder.TotalSeconds, SharedName);
